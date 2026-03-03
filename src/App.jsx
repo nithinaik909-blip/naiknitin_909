91762c80208562ded2f0493796b0955aab3fa75f
nithinaik909-blip <nithinaik909@gmail.com>
2026-03-03 22:09:45 +0530

    Create App component for Object Finder application

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Object Finder Application</h1>
+            {/* Add your component code here */}
+        </div>
+    );
+};
+
+export default App;
